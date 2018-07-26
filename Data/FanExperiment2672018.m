271d6cdf205b1f75997e6ad4b956058f18d51ac3
Unknown <mattjcliffe@gmail.com>
2018-07-26 15:14:53 +0100

    Added test 3

--- Data/FanExperiment2672018.m
+++ Data/FanExperiment2672018.m
@@ -1,17 +1,27 @@
 clear all; 
 clc
 
-fname = 'AlphasenseData_26072018_2.csv';
+fname = 'AlphasenseData_26072018_1.csv';
 data = importfile(fname);
 dt = datetime(datenum(data.DateTime),'convertfrom','datenum');
+
+window = 20;
+
+pmr1_av1 = movmean(data.PMr1,[window 0]);
+pmr2_av1  = movmean(data.PMr2,[window 0]);
 
 
 figure(1)
 clf()
 subplot(311)
-plot(dt,data.PMr1,'k')
+scatter(dt,data.PMr1,3,'k')
 hold on 
-plot(dt,data.PMr2,'r')
+scatter(dt,data.PMr2,3,'r')
+hold on 
+plot(dt,pmr1_av1,'k','linewidth',3)
+hold on 
+plot(dt,pmr2_av1,'r','linewidth',3)
+
 ylabel('PMr [ug/m3]')
 grid('on')
 
@@ -27,4 +37,5 @@ plot(dt,data.FanVoltage)
 ylabel('Fan Voltage [V]')
 grid('on')
 
-print([fname(1:25),'.png'],'-dpng')
+print([fname(1:25),'movaverage.png'],'-dpng')
+
